d3a1dc968ca65d524bd08a6b2789f8b0af35f66b
Patrick Mavi <patrickgamu4@gmail.com>
2024-06-19 10:35:21 +0200

    commit

---
+++ resources/views/note/index.blade.php
@@ -0,0 +1,30 @@
+<x-layout>
+    <div class= "note-container">
+
+        <a href="{{ route('note.create') }}" class="new-note-btn">
+            New Note
+        </a>
+
+
+        <div class="notes">
+            @foreach ($notes as $note)
+                <div class="note">
+                    <div class="note-body">
+                        {{ Str::words($note->note, 30) }}
+
+                    </div>
+
+                    <div class="note-buttons">
+                        <a href="#" class="note-edit-button">View</a>
+                        <a href="#" class="note-edit-button">Edit</a>
+                        <button class="note-delete-button">Delete</button>
+
+                    </div>
+
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+
+</x-layout>
